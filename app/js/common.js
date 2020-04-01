0972c332e4912129b2c6442662c5acd11ff016c0
alex <alex_kobilianskiy@bigmir.net>
2020-04-01 12:31:18 +0300

    section intro complete

--- app/js/common.js
+++ app/js/common.js
@@ -61,6 +61,80 @@ $(document).ready(function(){
         $(this).children('.droppy__drop ').removeClass('droppy__drop--active')
     });
 
+    var $introSlider = $('.intro-slider').owlCarousel({
+        loop:true,
+        nav: false,
+        items: 1,
+        margin: 15,
+        dots: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        // mouseDrag: false,
+        touchDrag: false,
+        autoplay: true,
+    });
+
+    var $info = $('#intro-current');
+
+    $introSlider.on('changed.owl.carousel', function (e) {
+        var currentItem = e.item.index - 1;
+        $info.text('0'+currentItem);
+    });
+
+    $('img.svg').each(function(){
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+
+
+
+
+
+
+
+
+    /** START FORMS */
+    var uPhone = $('.user-phone');
+    uPhone.mask("+7 (999) 999-99-99",{autoclear: false});
+
+    uPhone.on('click', function (ele) {
+        var needelem = ele.target || event.srcElement;
+        needelem.setSelectionRange(4,4);
+        needelem.focus();
+    });
+
+    $.validate({
+        form : '.contact-form',
+        scrollToTopOnError: false
+    });
+
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
@@ -75,4 +149,5 @@ $(document).ready(function(){
         });
         return false;
     });
+    /** END FORMS */
 });
